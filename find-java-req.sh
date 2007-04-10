1ae8d1e3ff4d76892bd51e0b3f17dc38f1236e6b
Elan Ruusamäe <glen@pld-linux.org>
2007-04-10 22:00:07 +0000

    - cleanup unused code
    Changed files:
        find-java-prov.sh -> 1.2
        find-java-req.sh -> 1.6

--- find-java-req.sh
+++ find-java-req.sh
@@ -7,49 +7,6 @@
 
 export PATH="/sbin:/usr/sbin:/bin:/usr/bin:/usr/X11R6/bin"
 
-IGNORE_DEPS="@"
-BUILDROOT="/"
-
-# Loop over all args
-while :; do
-# Break out if there are no more args
-	case $# in
-	0)
-		break
-		;;
-	esac
-
-# Get the first arg, and shuffle
-	option=$1
-	shift
-
-# Make all options have two hyphens
-	orig_option=$option	# Save original for error messages
-	case $option in
-	--*) ;;
-	-*) option=-$option ;;
-	esac
-
-	case $option in
-	--buildroot)
-		BUILDROOT=$1
-		shift
-		;;
-	--ignore_deps)
-		IGNORE_DEPS=$1
-		shift
-		;;
-	--help)
-		echo $usage
-		exit 0
-		;;
-	*)
-		echo "$0: Unrecognized option: \"$orig_option\"; use --help for usage." >&2
-		exit 1
-		;;
-	esac
-done
-
 javaclassversion() {
 	local ver
 	classver=$(file "$@" | grep -o 'compiled Java class data, version [0-9.]*' | awk '{print $NF}' | sort -u)
@@ -82,4 +39,4 @@ for file in $(cat -); do
 		javadeps --requires --rpmformat --keywords $file
 	;;
 	esac
-done | sort -u | egrep -v \'$IGNORE_DEPS\'
+done | sort -u
