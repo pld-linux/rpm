b1344e993fcc4313e962e4fb36bf2ac4bb6632f9
Elan Ruusamäe <glen@pld-linux.org>
2007-04-10 21:44:42 +0000

    - rpm (at least AC-branch) now classifies itself java files
    Changed files:
        rpm-macros.java -> 1.3

--- rpm-macros.java
+++ rpm-macros.java
@@ -1,8 +1,2 @@
 %define	__java_provides	%{nil}
 %define	__java_requires	/usr/lib/rpm/java-find-requires
-
-# rpm itself doesn't recognize .jar and .class
-%define	_use_internal_dependency_generator  0
-
-%define	__find_provides    %{__java_provides}
-%define	__find_requires    %{__java_requires}
