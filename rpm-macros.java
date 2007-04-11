1d5f908f173d63c3146b427f741424f56e426019
Elan Ruusamäe <glen@pld-linux.org>
2007-04-11 13:40:58 +0000

    - pass $RPM_BUILD_ROOT for find-* scripts
    Changed files:
        rpm-macros.java -> 1.5

--- rpm-macros.java
+++ rpm-macros.java
@@ -1,2 +1,2 @@
-%define	__java_provides	/usr/lib/rpm/java-find-provides
-%define	__java_requires	/usr/lib/rpm/java-find-requires
+%define	__java_provides	env RPM_BUILD_ROOT=%{buildroot} /usr/lib/rpm/java-find-provides
+%define	__java_requires	env RPM_BUILD_ROOT=%{buildroot} /usr/lib/rpm/java-find-requires
