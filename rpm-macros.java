05bbe7ac239e7c0c751e95a603b46ee6380389ee
Jakub Bogusz <qboosh@pld-linux.org>
2007-05-22 20:01:47 +0000

    - no provides script currently on HEAD
    Changed files:
        rpm-macros.java -> 1.6

--- rpm-macros.java
+++ rpm-macros.java
@@ -1,2 +1,2 @@
-%define	__java_provides	env RPM_BUILD_ROOT=%{buildroot} /usr/lib/rpm/java-find-provides
+%define	__java_provides	%{nil}
 %define	__java_requires	env RPM_BUILD_ROOT=%{buildroot} /usr/lib/rpm/java-find-requires
