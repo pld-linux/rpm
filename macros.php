93cfcd708ad6f6a93b5b19e32605e2c33363e115
Elan Ruusamäe <glen@pld-linux.org>
2011-11-28 11:34:50 +0000

    - package alternative php requires collector (it's a bit buggy so not eanbled default)
    Changed files:
        macros.php -> 1.3

--- macros.php
+++ macros.php
@@ -1,2 +1,3 @@
 %define	__php_provides	/usr/lib/rpm/php.prov
-%define	__php_requires	env PHP_MIN_VERSION=%{?php_min_version} /usr/lib/rpm/php.req
+# define 'php_req_new' in ~/.rpmmacros to use php version of req finder
+%define	__php_requires	env PHP_MIN_VERSION=%{?php_min_version} /usr/lib/rpm/php.req%{?php_req_new:.php}
